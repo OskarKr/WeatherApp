4f4ff8d50c5f22f3ec3742619ec549d5e4765123
Oskar Krolik <oskar.krolik@microsoft.wsei.edu.pl>
2023-01-19 07:27:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const cityName = document.querySelector('.city-name');
+const warning = document.querySelector('.warning');
+const photo = document.querySelector('.photo');
+const weather = document.querySelector('.weather');
+const temperature = document.querySelector('.temperature');
+const humidity = document.querySelector('.humidity');
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = '&appid=a0cbc72b6bbfbe79fff146c2b3995c15';
+const API_UNITS = '&units=metric';
